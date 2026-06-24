fda3aa1feae486c20074b7b222a7ae214e7785a3
Musely <jboysamuel08@gmail.com>
2026-06-24 04:41:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const phoneChange = document.getElementById("phoneChange");
 const emailAdd = document.getElementById("emailAdd");
 const emailChange = document.getElementById("emailChange");
 
-forgetPass.addEventListener("click", switch);
+//forgetPass.addEventListener("click", switch);
 
-function switch(){
-    window.location.href = "fordetten-password.html";
-}
+//function switch(){
+ //   window.location.href = "fordetten-password.html";
+//}
 
 fp2.onclick = function (){
     phoneChange.textContent = "Enter your Username or email address";
