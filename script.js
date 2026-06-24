e97852d354ce85928443fb2d0d3cc7de7883f140
Musely <jboysamuel08@gmail.com>
2026-06-24 04:28:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const backBtn = document.getElementById("backBtn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const forgetPass = document.querySelector(".fp");
+const phoneChange = document.getElementById("phoneChange");
+const emailAdd = document.getElementById("emailAdd");
+const emailChange = document.getElementById("emailChange");
 
 forgetPass.addEventListener("click", switch);
 
