af2645320d468013ea58b8e70495028cb8e2a530
Musely <jboysamuel08@gmail.com>
2026-06-24 04:21:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use stritct';
 
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 const forgetPass = document.querySelector(".fp");
 
 forgetPass.addEventListener("click", switch);
