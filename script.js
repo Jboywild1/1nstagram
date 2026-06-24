22876e1a42859d5aece2a9efba50ce8e9b458f70
Musely <jboysamuel08@gmail.com>
2026-06-24 04:13:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use stritct';
 
+const forgetPass = document.querySelector(".fp");
+
+forgetPass.addEventListener("click", switch);
+
+function switch(){
+    window.location.href = "fordetten-password.html";
+}
+
